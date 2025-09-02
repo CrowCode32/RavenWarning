b9cccdf1a19309fd2c50766e9eb276c1de00231f
MikazukiAugust <andrewzito2005@gmail.com>
2025-09-02 16:53:30 -0600

    Crow model added - I custom made one. Small problems with the collisions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame

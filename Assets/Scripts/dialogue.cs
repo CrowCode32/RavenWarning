ec2e4ece1d44013678a70751c0f5201413d001ff
CrowCode32 <crowsofmoye@gmail.com>
2025-09-10 16:01:07 -0500

    Basic Dialogue
    Added basic dialogue functionality. Dialogue appears one character at a time and the player can progress line by line. The lines are read from a text file.

---
+++ Assets/Scripts/dialogue.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using TMPro;
+using System.IO;
+using System.Collections;
+
+public class dialogue : MonoBehaviour
+{
+    [SerializeField] TextAsset dialogueFile;
+    public TextMeshProUGUI textField;
+    public string[] lines;
+    public float textSpeed;
+
+    int index;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textField.text = string.Empty;
+        startDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            if (textField.text == lines[index])
+            {
+                nextLine();
+            } else
+            {
+                StopAllCoroutines();
+                textField.text = lines[index];
+            }
+        }
+    }
+
+    void startDialogue()
+    {
+        index = 0;
+        
+        string allText = dialogueFile.text;
+        lines = allText.Split("\n");
+
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in lines[index].ToCharArray())
+        {
+            textField.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    
+    void nextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textField.text = string.Empty;
+            StartCoroutine(TypeLine());
+        } else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

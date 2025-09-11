6dd187f7cf9f2ffba67b0079ffc4b398ef3b82f9
CrowCode32 <crowsofmoye@gmail.com>
2025-09-11 18:37:32 -0500

    Bug Fixes, Forest Progress
    Reimplemented player animation and worked on forest. Haven't fixed bug with forest floor.

--- Assets/Scripts/NPCs/forestLord.cs
+++ Assets/Scripts/NPCs/forestLord.cs
@@ -11,7 +11,6 @@ public class forestLord : MonoBehaviour
             GameManager.instance.lordInformed("Forest");
         }
 
-        isTriggered = false;
         //Destroy(gameObject);
     }
 

d80208de2b898e5dbfe7dd7b691d624b03f6fd6a
CrowCode32 <crowsofmoye@gmail.com>
2025-09-27 19:35:06 -0500

    Load/Start Issues 2
    Hit commit too early. Commented out relevant code because it introduced a bug which is arguably a bigger issue than load doing nothing.

--- Assets/Scripts/buttonFunctions.cs
+++ Assets/Scripts/buttonFunctions.cs
@@ -20,7 +20,7 @@ public class buttonFunctions : MonoBehaviour
 
     public void startGame()
     {
-        /*GameManager.instance.NewGame();
+        //GameManager.instance.NewGame();
         Debug.Log("Game starting...");
         GameManager.instance.gameStarted = true;
 
@@ -29,11 +29,12 @@ public class buttonFunctions : MonoBehaviour
         GameManager.instance.playerHUD.SetActive(true);
 
         GameManager.instance.UpdateTrinketDropdown();
-        GameManager.instance.UpdateFeatherDropdown();*/
+        GameManager.instance.UpdateFeatherDropdown();
     }
 
     public void load()
     {
+        //Not functional yet
         /*GameManager.instance.LoadGame();
         Debug.Log("Game loading...");
         GameManager.instance.gameStarted = true;
